bc483d99a3224bfcca48671e7a43b6677f6de829
jeong eun <lje7370@gamil.com>
2025-04-18 12:27:57 +0900

    프로젝트 초기 세팅

---
+++ src/main/java/org/project/wherego/WhereGoApplication.java
@@ -0,0 +1,13 @@
+package org.project.wherego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhereGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhereGoApplication.class, args);
+    }
+
+}
