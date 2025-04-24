83b44b279c4e10c96af3620d8a936318c14205e4
yuyeseong <yuyeseong@naver.com>
2025-04-24 12:44:05 +0900

    jeongeun pull and fix version

--- src/main/java/org/project/wherego/checklist/dto/CheckListGroupDto.java
+++ src/main/java/org/project/wherego/checklist/dto/CheckListGroupDto.java
@@ -1,14 +1,4 @@
 package org.project.wherego.checklist.dto;
 
-import lombok.*;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class CheckListGroupDto {
-    private Long id;
-    private String title;
-
 }
